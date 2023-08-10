f754268c8c2a089054955581eeb132074f8dbbc9
Maxim <maximagaffonnovv@gmail.com>
2023-08-10 12:46:26 +0300

    create app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>Здесь скоро будет Skypro.Music</span>
+      </header>
+    </div>
+  );
+}
+
+export default App;
